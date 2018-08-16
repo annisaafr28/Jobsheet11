576bb645b0888c57253e7fb7a747ab3b1698833f
DELL <DELL@DELL-PC>
2018-08-17 01:08:15 +0700

    tambah konversi

--- src/j11/y.java
+++ src/j11/y.java
@@ -6,19 +6,20 @@ import java.util.Scanner;
 
 public class y {
     
-       private static String tampilInput() {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Masukkan Kalimat : ");
-        String kalimat = scanner.nextLine();
-        System.out.println("Kalimat Asli : " + kalimat);
+       private static String vocal2Angka(String kalimat) {
+        char[][] arConvert =
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+    
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
         
         return kalimat;
        }
         
     public static void main(String[] args) {
         String identitas = "nama lengkap / kelas / no";
-        String kalimat = tampilInput();
+        
         
         
     }

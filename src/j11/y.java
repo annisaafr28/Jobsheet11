5eb2241ff64a3fd3effaa47af177a1ba075db849
DELL <DELL@DELL-PC>
2018-08-17 01:10:16 +0700

    tambah tampil per kata

--- src/j11/y.java
+++ src/j11/y.java
@@ -6,15 +6,13 @@ import java.util.Scanner;
 
 public class y {
     
-       private static String vocal2Angka(String kalimat) {
-        char[][] arConvert =
-        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
-    
-        kalimat = kalimat.toLowerCase();
-        for(int i=0;i<arConvert.length;i++)
-            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+       private static void tampilPerKata(String kalimat, String convert) {
+        String[] arrKal = kalimat.split(" ");
+        String[] arrCon = convert.split(" ");
         
-        return kalimat;
+        for(int i=0;i<arrKal.length;i++)
+            System.out.println(arrKal[i]+" => "+arrCon[i]);
+    
        }
         
     public static void main(String[] args) {

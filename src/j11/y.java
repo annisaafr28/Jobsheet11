641831fb7151a9d2526df5ef077fa326b80ff6b7
DELL <DELL@DELL-PC>
2018-08-17 00:48:17 +0700

    coba method format

--- src/j11/y.java
+++ src/j11/y.java
@@ -1,6 +1,8 @@
 
 package j11;
 
+import java.util.Locale;
+
 public class y {
     public static void main(String[] args) {
        String y = "";
@@ -9,24 +11,10 @@ public class y {
        String r = "operasi";
        String t = "    operasi string    ";
        
-       System.out.println("karakter mulai posisi 3 di  variable x : " + x.substring(3));
+       
+        double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
         
-        System.out.println("karakter dari posisi 3-5 di variable x : " + x.substring(3,6));
-        
-        String tclean = t.trim();
-        System.out.println("isi variable tolean : " + t.trim());
-        System.out.println("rubah 'i' jadi 'E' di variable tclean : " + tclean.replace("i","E"));
-        
-        
-        
-        String[] arrT = tclean.split(" ");
-        System.out.println("pecah kata di variable tclean : ");
-        for(int i=0;i<arrT.length;i++)
-            System.out.println(arrT[i]);
-            
-        String[] arrTr = tclean.split("r");
-        System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
-        for(int i=0;i<arrTr.length;i++)
-            System.out.println(arrTr[i]);
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"), "uang saya Rp %,.2f", d));
     }
 }

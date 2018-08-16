e69c00ba51beb593fb611a21bfe1a6be8294592d
DELL <DELL@DELL-PC>
2018-08-17 00:33:02 +0700

    coba method substring, replace, & split

--- src/j11/y.java
+++ src/j11/y.java
@@ -8,16 +8,24 @@ public class y {
        String z = "Operasi";
        String r = "operasi";
        
-       System.out.println("Pada x terdapat huruf 'a' : " + x.contains("a"));
+       System.out.println("karakter mulai posisi 3 di  variable x : " + x.substring(3));
         
-        System.out.println("Isi variable x besar semua : " + x.toUpperCase());
-        System.out.println("Isi variable x kecil semua : " + x.toLowerCase());
+        System.out.println("karakter dari posisi 3-5 di variable x : " + x.substring(3,6));
         
-        String t = "    operasi string    ";
-        System.out.println("Isi variable t : \""+ t +"\"");
-        System.out.println("isi variable t tanpa spasi : \""+ t.trim() +"\"");
+        String tclean = t.trim();
+        System.out.println("isi variable tolean : " + t.trim());
+        System.out.println("rubah 'i' jadi 'E' di variable tclean : " + tclean.replace("i","E"));
         
-        System.out.println("Gabungkan isi variable x dan r : "+ x.concat(r));
-
+        
+        
+        String[] arrT = tclean.split(" ");
+        System.out.println("pecah kata di variable tclean : ");
+        for(int i=0;i<arrT.length;i++)
+            System.out.println(arrT[i]);
+            
+        String[] arrTr = tclean.split("r");
+        System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
+        for(int i=0;i<arrTr.length;i++)
+            System.out.println(arrTr[i]);
     }
 }

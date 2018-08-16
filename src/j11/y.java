d8f8a56bf6029651a229ce7844cbe1c1c7c203b0
DELL <DELL@DELL-PC>
2018-08-17 00:26:47 +0700

    coba method equals & equalsIgnoreCase

--- src/j11/y.java
+++ src/j11/y.java
@@ -3,10 +3,22 @@ package j11;
 
 public class y {
     public static void main(String[] args) {
-        String x = "Operasi";
-        System.out.println("Isi variable x : " + x);
-        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+       String y = "";
+       String x = "Operasi";
+       
+        System.out.println("Isi variable y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
         
-        System.out.println("x adalah kosong : " + x.isEmpty());
+        System.out.println("Isi x sama dengan y : " + x.equals(y));
+        
+        String z = "Operasi";
+        System.out.println("Isi variable z : "+ z);
+        System.out.println("Isi x sama dengan z (Case Sensitive) : " + x.equals(z));
+        
+        String r = "operasi";
+        System.out.println("Isi variable r : " + r);
+        System.out.println("Isi x sama dengan r (Case Sensitive) : " + x.equals(r));
+        System.out.println("Isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
+
     }
 }

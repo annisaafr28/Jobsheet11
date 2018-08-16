f6b6853966a6f87a3a0efad164a26481a3431fdd
DELL <DELL@DELL-PC>
2018-08-16 23:10:05 +0700

    Initial Commit

---
+++ src/j11/y.java
@@ -0,0 +1,9 @@
+
+package j11;
+
+public class y {
+    public static void main(String[] args) {
+        String identitas = "nama lengkap / kelas / no";
+        System.out.println("Identitas : " + identitas);
+    }
+}
